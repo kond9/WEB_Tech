623642606375ef50a303a30caaaa75ac9afb4d5b
kond9 <plenkin.2018@gmail.com>
2023-12-27 01:32:55 +0300

    add paginator

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
@@ -6,43 +8,47 @@ QUESTIONS = [
         'id': i,
         'title': f'Question {i}',
         'content': f'Long lorem ipsum {i}'
-    } for i in range(160)
+    } for i in range(40)
 ]
 
 
-def paginate(objects, page, per_page=30):
-    paginator = Paginator(objects, per_page)
-
-    return paginator.page(page)
+def paginate(objects_list, request, per_page=30):
+    paginator = Paginator(objects_list, per_page)
+    page = request.GET.get('page', 1)
+    if str(page).isdigit() and int(page) <= int(math.ceil(len(objects_list) / per_page)):
+        return paginator.page(page)
+    return paginator.page(1)
 
 
 # Create your views here.
 def index(request):
-    page = request.GET.get('page', 1)
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, request)})
 
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
     return render(request, 'question.html', {'question': item})
 
+
 def hot(request):
-    page = request.GET.get('page', 1)
-    return render(request, 'hot.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'hot.html', {'questions': paginate(QUESTIONS, request)})
+
 
 def tag(request, tag_name):
-    page = request.GET.get('page', 1)
-    return render(request, 'tag.html', {'questions': paginate(QUESTIONS, page), 'tag':tag_name})
+    return render(request, 'tag.html', {'questions': paginate(QUESTIONS, request), 'tag': tag_name})
+
 
 def login(request):
     return render(request, 'login.html')
 
+
 def signup(request):
     return render(request, 'signup.html')
+
 
 def ask(request):
     return render(request, 'ask.html')
 
+
 def settings(request):
     return render(request, 'settings.html')
-
