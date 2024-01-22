09ab2d8ea42821d5eb49044a2db9fac02782688c
kond9 <plenkin.2018@gmail.com>
2024-01-23 02:04:00 +0300

    code after sem 4

---
+++ app/forms.py
@@ -0,0 +1,43 @@
+import math
+
+from django.contrib import auth
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.http import Http404
+
+from .models import Question, Answer, Tag, Profile, QuestionManager
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.urls import reverse
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(min_length=4, widget=forms.PasswordInput)
+
+    def clean_password(self):
+        data = self.cleaned_data['password']
+        if data == 'wrongpass':
+            raise ValidationError('Wrong password')
+        return data
+
+
+class RegisterForm(forms.ModelForm):
+    password=forms.CharField(min_length=4, widget=forms.PasswordInput)
+    password_check=forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+    def clean(self):
+        password=self.cleaned_data['password']
+        password_check=self.cleaned_data['password_check']
+        if password != password_check:
+            raise ValidationError('Password do not match')
+
+    def save(self, **kwargs):
+        self.cleaned_data.pop('password_check')
+        return User.objects.create_user(**self.cleaned_data)
--- app/views.py
+++ app/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 
-
+from .forms import LoginForm, RegisterForm
 from .models import Question, Answer, Tag, Profile, QuestionManager
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 def paginate(objects_list, request, per_page=20):
     paginator = Paginator(objects_list, per_page)
@@ -21,10 +22,10 @@ def paginate(objects_list, request, per_page=20):
 
 
 # Create your views here.
+@login_required(login_url='log_in/', redirect_field_name='continue')
 def index(request):
     questions = Question.mymanager.get_latest_questions()
     return render(request, 'index.html', {'questions': paginate(questions, request)})
-
 
 
 def question(request, question_id):
@@ -49,23 +50,42 @@ def tag(request, tag_name):
 def log_in(request):
     print(request.GET)
     print(request.POST)
+    if request.method == 'GET':
+        login_form = LoginForm()
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            print('Successfully logged in')
-            return redirect(reverse('index'))
-    return render(request, 'login.html')
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user = authenticate(request, **login_form.cleaned_data)
+            print(user)
+            if user is not None:
+                login(request, user)
+                print('Successfully logged in')
+                return redirect(request.GET.get('continue', '/'))
+            else:
+                login_form.add_error(None, "Wrong password or user does not exist.")
+    return render(request, 'login.html', context={"form": login_form})
+
 
 def log_out(request):
     auth.logout(request)
     return redirect(reverse('login'))
 
+
 def signup(request):
-    return render(request, 'signup.html')
+    print(request.GET)
+    print(request.POST)
+    if request.method == 'GET':
+        user_form = RegisterForm()
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            print(user)
+            if user:
+                return redirect(reverse('index'))
+            else:
+                user_form.add_error(None, "User saving error!")
+    return render(request, 'signup.html', context={"form": user_form})
 
 
 def ask(request):
@@ -74,4 +94,3 @@ def ask(request):
 
 def settings(request):
     return render(request, 'settings.html')
-
